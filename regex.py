d3e1d7302cd42026916f52eb10cec7e3ffff7d8c
Morten Zdrenka Christensen <mortenzc42@gmail.com>
2020-04-04 18:37:43 +0200

    More playing with regex.

--- regex.py
+++ regex.py
@@ -1,27 +1,30 @@
 import re
 import os
 
-{os.path.sep}
+
 
 base_path = f"C:{os.path.sep}Users{os.path.sep}Morten{os.path.sep}Projects{os.path.sep}file-backup{os.path.sep}testsubject{os.path.sep}source"
-
 destination_path = f"C:{os.path.sep}Users{os.path.sep}Morten{os.path.sep}Projects{os.path.sep}file-backup{os.path.sep}testsubject{os.path.sep}destination"
-
 changed_path = f"C:{os.path.sep}Users{os.path.sep}Morten{os.path.sep}Projects{os.path.sep}file-backup{os.path.sep}testsubject{os.path.sep}source{os.path.sep}folder1{os.path.sep}folder2{os.path.sep}sub-file1.txt"
 
-escaped_base = re.escape(base_path)
-escaped_changed_path = re.escape(changed_path)
-print(escaped_base)
-print(escaped_changed_path)
+# This turns the annoying "\" into "/", in case we are on windows
+base_path = base_path.replace(os.path.sep, "/")
+changed_path = changed_path.replace(os.path.sep, "/")
 
+# print(base_path)
+# print(changed_path)
 
-# if (re.match(f"^{re.escape(base_path)}", re.escape(changed_path))):
-#     print(True)
-# else:
-#     print(False)
+regex = re.compile(f"({base_path})(.*)")
 
-
-if re.match('^hello', 'helloworld'):
-    print(True)
+if re.match(regex, changed_path):
+    match = re.search(regex, changed_path)
+    print(match.group())
+    print(match.group(0))
+    print(match.group((1)))
 else:
     print(False)
+
+
+base_path = base_path.replace("/", os.path.sep)
+changed_path = changed_path.replace("/", os.path.sep)
+# print(base_path, "\n", changed_path)
