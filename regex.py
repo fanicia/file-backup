8bf465b1ab5c56ea46055459b48945d73fc2e4d7
Morten Zdrenka Christensen <mortenzc42@gmail.com>
2020-04-05 11:51:10 +0200

    Find a way to extract the sub-path from the changed path.

--- regex.py
+++ regex.py
@@ -1,8 +1,6 @@
 import re
 import os
 
-
-
 base_path = f"C:{os.path.sep}Users{os.path.sep}Morten{os.path.sep}Projects{os.path.sep}file-backup{os.path.sep}testsubject{os.path.sep}source"
 destination_path = f"C:{os.path.sep}Users{os.path.sep}Morten{os.path.sep}Projects{os.path.sep}file-backup{os.path.sep}testsubject{os.path.sep}destination"
 changed_path = f"C:{os.path.sep}Users{os.path.sep}Morten{os.path.sep}Projects{os.path.sep}file-backup{os.path.sep}testsubject{os.path.sep}source{os.path.sep}folder1{os.path.sep}folder2{os.path.sep}sub-file1.txt"
@@ -11,20 +9,10 @@ changed_path = f"C:{os.path.sep}Users{os.path.sep}Morten{os.path.sep}Projects{os
 base_path = base_path.replace(os.path.sep, "/")
 changed_path = changed_path.replace(os.path.sep, "/")
 
-# print(base_path)
-# print(changed_path)
+regex = re.compile(f"(?<={base_path})(.*)")
 
-regex = re.compile(f"({base_path})(.*)")
-
-if re.match(regex, changed_path):
-    match = re.search(regex, changed_path)
-    print(match.group())
-    print(match.group(0))
-    print(match.group((1)))
-else:
-    print(False)
-
+match = re.search(regex, changed_path)
+print(match.group())
 
 base_path = base_path.replace("/", os.path.sep)
 changed_path = changed_path.replace("/", os.path.sep)
-# print(base_path, "\n", changed_path)
